6b51c259803090c11a03a401d14a75205291d3af
imww <im.ww@foxmail.com>
2018-07-26 18:59:50 +0800

    cc

--- db/circleCategory.js
+++ db/circleCategory.js
@@ -1,3 +1,33 @@
 /**
  * Created by wei on 18-7-26.
  */
+'use strict';
+
+const mongoose = require('mongoose');
+const db=require('./db');
+const modelName='CircleCategory';
+
+const schema=new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    orderNum: {
+        type: Number,
+        required: true
+    }
+});
+
+schema.index( {name : 1}, { unique : true } );
+schema.index( {orderNum: 1}, { unique: true });
+
+schema.statics={
+
+};
+
+module.exports= {
+    load:function(){
+        let model = mongoose.model(modelName, schema);
+        console.log(`模块${modelName}被注册`);
+    }
+};
--- db/circleContent.js
+++ db/circleContent.js
@@ -1,3 +1,28 @@
 /**
  * Created by wei on 18-7-26.
  */
+const mongoose = require('mongoose');
+const db=require('./db');
+const modelName='CircleContent';
+
+const schema=new mongoose.Schema({
+    uid: {
+        type: String
+    },
+    circleCateGoryUid: {
+        type: String
+    },
+});
+
+// schema.index( {name : 1}, { unique : true } );
+
+schema.statics={
+
+};
+
+module.exports= {
+    load:function(){
+        let model = mongoose.model(modelName, schema);
+        console.log(`模块${modelName}被注册`);
+    }
+};
